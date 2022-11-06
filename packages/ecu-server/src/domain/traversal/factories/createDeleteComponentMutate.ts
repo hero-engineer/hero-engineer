6e54b4263a9923ebd56d2b8481cdfbcd177ff051
David Hérault <dherault@gmail.com>
2022-11-06 22:26:43 +0100

    Remove unused files

--- packages/ecu-server/src/domain/traversal/factories/createDeleteComponentMutate.ts
+++
@@ -1,9 +0,0 @@
-import { MutateType } from '../../../types'
-
-function createDeleteComponentMutate(): MutateType {
-  return (x: any, previousX: any) => {
-    (previousX || x).remove()
-  }
-}
-
-export default createDeleteComponentMutate
