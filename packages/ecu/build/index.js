d96a476ec73ad6907ac47214b1b52539bf1e28e9
David Hérault <dherault@gmail.com>
2022-11-16 00:11:35 +0100

    Create serve command, about to publish

---
+++ packages/ecu/build/index.js
@@ -0,0 +1,16 @@
+import { Command } from 'commander';
+import serve from 'ecu-server';
+const program = new Command();
+program
+    .name('ecu')
+    .description('Self-reprogrammation rocks!')
+    .version('0.0.0');
+program.command('serve')
+    .description('Starts an ecu-server instance')
+    // .argument('<string>', 'string to split')
+    // .option('--first', 'display just the first substring')
+    // .option('-s, --separator <char>', 'separator character', ',')
+    // .action((str, options) => {
+    .action(async () => {
+    await serve();
+});
