24da8141bd7171d86d7445d832fe2fc29014c040
David Hérault <dherault@gmail.com>
2022-11-22 22:24:24 +0100

    [eslint] Remove a react rule to allow ecu-display-name comments

--- packages/eslint-config-ecu/index.js
+++ packages/eslint-config-ecu/index.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: 'dherault-typescript',
   rules: {
     'react/no-unused-prop-types': 'warn',
+    'react/jsx-closing-bracket-location': 'off', // To allow ecu-display-name and other JSX comments
   },
 }
