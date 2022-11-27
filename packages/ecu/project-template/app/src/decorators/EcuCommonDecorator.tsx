f47d437f9f722603178e6c773845e4590d231e82
David Hérault <dherault@gmail.com>
2022-11-27 09:04:07 +0100

    [ecu] Update CommonDecorator

--- packages/ecu/project-template/app/src/decorators/EcuCommonDecorator.tsx
+++ packages/ecu/project-template/app/src/decorators/EcuCommonDecorator.tsx
@@ -3,7 +3,7 @@
  * EcuCommonDecorator.tsx is used to decorate any Ecu component
  * Its primary role is to inject the index.css into the Ecu iframe
 -- */
-import { ReactNode, useEffect } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 import indexCss from '../index.css?inline'
 
@@ -13,15 +13,23 @@ type EcuCommonDecoratorPropsType = {
 }
 
 function EcuCommonDecorator({ children, head }: EcuCommonDecoratorPropsType) {
+  const [styleElement, setStyleElement] = useState<HTMLStyleElement | null>(null)
 
   useEffect(() => {
     if (!head) return
+
+    if (styleElement) {
+      head.removeChild(styleElement)
+    }
 
     const style = document.createElement('style')
 
     style.innerHTML = indexCss
 
     head.appendChild(style)
+
+    setStyleElement(style)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [head])
 
   return children
