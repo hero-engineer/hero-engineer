433e04623135bc92253e5a5f3c65922e9fc5b127
David Hérault <dherault@gmail.com>
2022-11-28 15:07:29 +0100

    [server] Escape unicode characters

--- packages/ecu-server/src/domain/generate.ts
+++ packages/ecu-server/src/domain/generate.ts
@@ -1,5 +1,6 @@
+import { ParseResult } from '@babel/core'
 import generate from '@babel/generator'
 
 // Fixed in babel v8
 // @ts-expect-error
-export default generate.default as typeof generate
+export default ((astOrCode: ParseResult | string | null) => generate.default(astOrCode, { jsescOption: { minimal: true } })) as typeof generate // jsescOption: { minimal: true } to prevent escaping unicode characters
