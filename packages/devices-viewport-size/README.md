d47d08302bd7f220376b2c09910d8c992fe74bab
David Hérault <dherault@gmail.com>
2022-12-07 19:33:05 +0100

    [devices-viewport-size] Create package and publish it

---
+++ packages/devices-viewport-size/README.md
@@ -0,0 +1,19 @@
+# devices-viewport-size
+
+A list of common devices viewport sizes
+
+## Installation
+
+`npm i -S devices-viewport-size`
+
+## Usage
+
+```js
+import viewports from 'devices-viewport-size'
+
+console.log(viewports['iPhone SE'])
+```
+
+## LICENSE
+
+MIT
---
+++ packages/devices-viewport-size/src/index.ts
@@ -0,0 +1,70 @@
+export default {
+  Desktop: {
+    width: 1024,
+    height: 768,
+  },
+  'Desktop Large': {
+    width: 1920,
+    height: 1080,
+  },
+  'iPhone SE': {
+    width: 375,
+    height: 667,
+  },
+  'iPhone XR': {
+    width: 414,
+    height: 896,
+  },
+  'iPhone 12 Pro': {
+    width: 390,
+    height: 844,
+  },
+  'Pixel 5': {
+    width: 393,
+    height: 851,
+  },
+  'Galaxy S8+': {
+    width: 360,
+    height: 740,
+  },
+  'Galaxy S20 Ultra': {
+    width: 412,
+    height: 915,
+  },
+  'Pixel 2 XL': {
+    width: 411,
+    height: 823,
+  },
+  'iPad Air': {
+    width: 820,
+    height: 1180,
+  },
+  'iPad Mini': {
+    width: 768,
+    height: 1024,
+  },
+  'Surface Pro 7': {
+    width: 912,
+    height: 1368,
+  },
+  'Surface Duo': {
+    width: 540,
+    height: 720,
+  },
+  'Galaxy Fold': {
+    width: 280,
+    height: 653,
+  },
+  'Galaxy A51/71': {
+    width: 412,
+    height: 914,
+  },
+  'Nest Hub': {
+    width: 1024,
+    height: 600,
+  },
+  'Nest Hub Max': {
+    width: 1280,
+    height: 800,
+  },
+}
