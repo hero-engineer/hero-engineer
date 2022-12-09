f4214f35ad7330837e81548aaef1da7166f6e6b6
David Hérault <dherault@gmail.com>
2022-12-09 22:01:20 +0100

    [server] Serve node_modules files

--- packages/ecu-server/src/graphql/queries2/filesQuery.ts
+++ packages/ecu-server/src/graphql/queries2/filesQuery.ts
@@ -5,39 +5,16 @@ import { FileType } from '../../types.js'
 import { appPath } from '../../configuration.js'
 
 function filesQuery() {
-  const files: FileType[] = []
-  const srcPath = path.join(appPath, 'src')
-
-  function readDirectory(location: string,) {
-    fs.readdirSync(location).forEach(fileName => {
-      const filePath = path.join(location, fileName)
-
-      if (fs.statSync(filePath).isDirectory()) {
-        return readDirectory(filePath)
-      }
-
-      files.push({
-        path: filePath,
-        code: fs.readFileSync(filePath, 'utf8'),
-      })
-    })
-  }
-
-  readDirectory(srcPath)
-
   // const files: FileType[] = []
   // const srcPath = path.join(appPath, 'src')
-  // const nodeModulesPath = path.join(appPath, 'node_modules')
 
-  // function readDirectory(location: string, isNodeModules = false) {
+  // function readDirectory(location: string,) {
   //   fs.readdirSync(location).forEach(fileName => {
   //     const filePath = path.join(location, fileName)
 
   //     if (fs.statSync(filePath).isDirectory()) {
-  //       return readDirectory(filePath, isNodeModules)
+  //       return readDirectory(filePath)
   //     }
-
-  //     if (isNodeModules && !filePath.endsWith('.d.ts')) return
 
   //     files.push({
   //       path: filePath,
@@ -47,7 +24,30 @@ function filesQuery() {
   // }
 
   // readDirectory(srcPath)
-  // readDirectory(nodeModulesPath, true)
+
+  const files: FileType[] = []
+  const srcPath = path.join(appPath, 'src')
+  const nodeModulesPath = path.join(appPath, 'node_modules')
+
+  function readDirectory(location: string, isNodeModules = false) {
+    fs.readdirSync(location).forEach(fileName => {
+      const filePath = path.join(location, fileName)
+
+      if (fs.statSync(filePath).isDirectory()) {
+        return readDirectory(filePath, isNodeModules)
+      }
+
+      if (isNodeModules && !filePath.endsWith('.d.ts')) return
+
+      files.push({
+        path: filePath,
+        code: fs.readFileSync(filePath, 'utf8'),
+      })
+    })
+  }
+
+  readDirectory(srcPath)
+  readDirectory(nodeModulesPath, true)
 
   return files
 }
