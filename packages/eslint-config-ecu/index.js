f2c296f8db387b2faf4badaf6a831a5b86b97b87
David Hérault <dherault@gmail.com>
2022-12-11 23:31:59 +0100

    [eslint] Edit rules and publish

--- packages/eslint-config-ecu/index.js
+++ packages/eslint-config-ecu/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: 'dherault-typescript',
   rules: {
+    'default-param-last': 'off',
     'react/no-unused-prop-types': 'warn',
     'react/jsx-closing-bracket-location': 'off', // To allow ecu-display-name and other JSX comments
   },
