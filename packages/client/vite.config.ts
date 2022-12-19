cf696f8b691e54715349e418d8cd1e3c857b1436
David Hérault <dherault@gmail.com>
2022-12-19 23:15:56 +0100

    [server] Publish patch

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import { typescriptPaths } from 'rollup-plugin-typescript-paths'
-// import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 const isProductionBuild = process.env.PRODUCTION_BUILD === 'true'
 
@@ -20,17 +19,16 @@ export default defineConfig({
   build: {
     minify: isProductionBuild,
     sourcemap: isProductionBuild,
-    outDir: 'build',
     emptyOutDir: isProductionBuild,
+    outDir: 'build',
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
-      fileName: 'hero-engineer-client.js',
+      fileName: 'hero-engineer-client',
     },
     rollupOptions: {
       plugins: [
         typescriptPaths({ preserveExtensions: true }),
-        // nodePolyfills(),
       ],
       external: ['react', 'react-dom'],
       output: {
