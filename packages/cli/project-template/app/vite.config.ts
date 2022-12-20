a0ce7990d7afb2b9843a7cf24bc1be906e107380
David Hérault <dherault@gmail.com>
2022-12-20 22:38:45 +0100

    [cli] Edit project template

--- packages/cli/project-template/app/vite.config.ts
+++ packages/cli/project-template/app/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cwd from 'vite-plugin-cwd' // This is needed by Hero Engineer to resolve the current working directory
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cwd()],
   optimizeDeps: {
     include: ['react-dom'],
   },
