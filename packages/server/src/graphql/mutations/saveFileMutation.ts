4809c0f737269076627a3ac3e627526c04831bed
David Hérault <dherault@gmail.com>
2022-12-21 02:37:35 +0100

    [server] Ensure dir existance in saveFileMutation

--- packages/server/src/graphql/mutations/saveFileMutation.ts
+++ packages/server/src/graphql/mutations/saveFileMutation.ts
@@ -1,4 +1,5 @@
 import fs from 'node:fs'
+import path from 'node:path'
 
 import { HistoryMutationReturnType } from '../../types.js'
 
@@ -11,6 +12,7 @@ type SaveFileMutationArgsType = {
 }
 
 async function saveFileMutation(_: any, { filePath, code, commitMessage }: SaveFileMutationArgsType): Promise<HistoryMutationReturnType<boolean>> {
+  fs.mkdirSync(path.dirname(filePath), { recursive: true })
   fs.writeFileSync(filePath, code, 'utf8')
 
   return {
