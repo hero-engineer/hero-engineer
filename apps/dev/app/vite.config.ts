6d5d3c434a9247e9357777c028fa21466fb4a304
David Hérault <dherault@gmail.com>
2022-12-22 06:56:46 +0100

    [hero-engineer] Update selector .foo in index.css

--- apps/dev/app/vite.config.ts
+++ apps/dev/app/vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'node:path'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cwd from 'vite-plugin-cwd' // This is needed by Hero Engineer to resolve the current working directory
@@ -7,5 +9,6 @@ export default defineConfig({
   plugins: [react(), cwd()],
   optimizeDeps: {
     include: ['react-dom'],
+    exclude: ['@hero-engineer/client', path.join(__dirname, '../../../packages/client/build/hero-engineer-client.js')],
   },
 })
